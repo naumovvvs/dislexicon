3afaa7c23f237b63921521ea8d6163b4fec1d131
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2022-04-26 16:16:27 +0200

    Initial commit and project creation

---
+++ src/main/java/mk/ukim/finki/ahci/dislexicon/DislexiconApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ahci.dislexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DislexiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DislexiconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/ahci/dislexicon/DislexiconApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ahci.dislexicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DislexiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

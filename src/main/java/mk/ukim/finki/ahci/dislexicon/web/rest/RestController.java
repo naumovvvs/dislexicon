a4d6e635efa4e5b3b6bf0b082081387c5206550b
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2022-07-02 18:44:03 +0200

    Modify pages, add new REST paths, add new image, modify logic and appearance

--- src/main/java/mk/ukim/finki/ahci/dislexicon/web/rest/RestController.java
+++ src/main/java/mk/ukim/finki/ahci/dislexicon/web/rest/RestController.java
@@ -1,9 +1,13 @@
 package mk.ukim.finki.ahci.dislexicon.web.rest;
 
 import mk.ukim.finki.ahci.dislexicon.model.Item;
+import mk.ukim.finki.ahci.dislexicon.model.MathQuestion;
 import mk.ukim.finki.ahci.dislexicon.model.Question;
+import mk.ukim.finki.ahci.dislexicon.model.Riddle;
 import mk.ukim.finki.ahci.dislexicon.repository.ItemRepository;
+import mk.ukim.finki.ahci.dislexicon.repository.MathQuestionRepository;
 import mk.ukim.finki.ahci.dislexicon.repository.QuestionRepository;
+import mk.ukim.finki.ahci.dislexicon.repository.RiddleRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,10 +18,14 @@ import java.util.List;
 public class RestController {
     private final ItemRepository itemRepository;
     private final QuestionRepository questionRepository;
+    private final RiddleRepository riddleRepository;
+    private final MathQuestionRepository mathQuestionRepository;
 
-    public RestController(ItemRepository itemRepository, QuestionRepository questionRepository) {
+    public RestController(ItemRepository itemRepository, QuestionRepository questionRepository, RiddleRepository riddleRepository, MathQuestionRepository mathQuestionRepository) {
         this.itemRepository = itemRepository;
         this.questionRepository = questionRepository;
+        this.riddleRepository = riddleRepository;
+        this.mathQuestionRepository = mathQuestionRepository;
     }
 
     @GetMapping("/pictureWord")
@@ -28,5 +36,15 @@ public class RestController {
     @GetMapping("/questions")
     public List<Question> getAllQuestions() {
         return this.questionRepository.findAll();
+    }
+
+    @GetMapping("/riddles")
+    public List<Riddle> getAllRiddles() {
+        return this.riddleRepository.findAll();
+    }
+
+    @GetMapping("/math")
+    public List<MathQuestion> getAllMathQuestions() {
+        return this.mathQuestionRepository.findAll();
     }
 }
